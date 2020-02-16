3a8dfdf6f87669f158411b6a6a33912ce73cd957
Jordi Chulia <jorchube.dev@gmail.com>
2020-02-16 18:41:40 +0100

    Added Roman numerals kata

---
+++ RomanNumeralsCSharp/RomanNumeralsCSharp/Romans.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace RomanNumeralsCSharp
+{
+    public class Romans
+    {
+        static RomansConversionTable conversion_table;
+
+        public static string ToRoman(int value)
+        {
+            conversion_table = new RomansConversionTable();
+
+            return DecimalToRoman(value);
+        }
+
+        public static int ToDecimal(string value)
+        {
+            conversion_table = new RomansConversionTable();
+
+            return RomanToDecimal(value);
+        }
+
+        private static int RomanToDecimal(string value)
+        {
+            if (value.Length == 1) {
+                return conversion_table.ToDecimal(value);
+            }
+
+            if (value.Length == 2 && conversion_table.HasRomanKey(value)) {
+                return conversion_table.ToDecimal(value);
+            }
+
+            if (value.Length > 2 && conversion_table.HasRomanKey(value.Substring(0, 2))) {
+                return conversion_table.ToDecimal(value.Substring(0, 2)) + RomanToDecimal(value.Substring(2));
+            }
+
+            return conversion_table.ToDecimal(value.Substring(0, value.Length-1)) + RomanToDecimal(value.Substring(1));
+        }
+
+        private static string DecimalToRoman(int value)
+        {
+            int substractor;
+
+            if (conversion_table.HasDecimalKey(value)) {
+                return conversion_table.ToRoman(value);
+            }
+
+            substractor = HighestSubstractor(value);
+
+            return conversion_table.ToRoman(substractor) + DecimalToRoman(value - substractor);
+        }
+
+        private static int HighestSubstractor(int value)
+        {
+            int[] keys = conversion_table.DecimalKeys();
+
+            SortKeysArrayAscending(keys);
+
+            foreach (int key in keys) {
+                if (value > key) {
+                    return key;
+                }
+            }
+
+            throw new UnableToFindDivisorException(string.Format("Unable to find substractor for {0}", value));
+        }
+
+        private static void SortKeysArrayAscending(int[] keys)
+        {
+            Array.Sort(keys);
+            Array.Reverse(keys);
+        }
+
+        private class UnableToFindDivisorException : Exception
+        {
+            public UnableToFindDivisorException(string message) : base(message)
+            {
+            }
+        }
+    }
+
+
+    class RomansConversionTable
+    {
+        ValueTuple<int, string>[] equivalences = {
+            (1, "I"),
+            (4, "IV"),
+            (5, "V"),
+            (9, "IX"),
+            (10, "X"),
+            (40, "XL"),
+            (50, "L"),
+            (90, "XC"),
+            (100, "C"),
+            (400, "CD"),
+            (500, "D"),
+            (900, "CM"),
+            (1000, "M")
+        };
+
+        Dictionary<int, string> decimal_keys_table;
+        Dictionary<string, int> roman_keys_table;
+
+        public RomansConversionTable()
+        {
+            decimal_keys_table = new Dictionary<int, string>();
+            roman_keys_table = new Dictionary<string, int>();
+
+            fillKeysTables();
+        }
+
+        public bool HasDecimalKey(int key)
+        {
+            return decimal_keys_table.ContainsKey(key);
+        }
+
+        public int[] DecimalKeys()
+        {
+            int[] keys = new int[decimal_keys_table.Keys.Count];
+
+            decimal_keys_table.Keys.CopyTo(keys, 0);
+
+            return keys;
+        }
+
+        public string ToRoman(int key)
+        {
+            string value;
+
+            decimal_keys_table.TryGetValue(key, out value);
+
+            return value;
+        }
+
+        public int ToDecimal(string key)
+        {
+            int value;
+
+            roman_keys_table.TryGetValue(key, out value);
+
+            return value;
+        }
+
+        private void fillKeysTables()
+        {
+            foreach (ValueTuple<int, string> tuple in equivalences) {
+                decimal_keys_table.Add(tuple.Item1, tuple.Item2);
+                roman_keys_table.Add(tuple.Item2, tuple.Item1);
+            }
+        }
+
+        internal bool HasRomanKey(string key)
+        {
+            return roman_keys_table.ContainsKey(key);
+        }
+    }
+}
---
+++ RomanNumeralsCSharp/RomanNumeralsTests/RomanNumeralTests.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+
+using RomanNumeralsCSharp;
+
+namespace RomanNumeralsTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldConvertDirectEquivalentDecimalsToRomans()
+        {
+            Assert.AreEqual("I", Romans.ToRoman(1));
+            Assert.AreEqual("V", Romans.ToRoman(5));
+            Assert.AreEqual("X", Romans.ToRoman(10));
+            Assert.AreEqual("L", Romans.ToRoman(50));
+            Assert.AreEqual("C", Romans.ToRoman(100));
+            Assert.AreEqual("D", Romans.ToRoman(500));
+            Assert.AreEqual("M", Romans.ToRoman(1000));
+        }
+
+        [Test]
+        public void ShouldConvertDirectSubstractedDecimalsToRomans()
+        {
+            Assert.AreEqual("IV", Romans.ToRoman(4));
+            Assert.AreEqual("IX", Romans.ToRoman(9));
+            Assert.AreEqual("XL", Romans.ToRoman(40));
+            Assert.AreEqual("XC", Romans.ToRoman(90));
+            Assert.AreEqual("CD", Romans.ToRoman(400));
+            Assert.AreEqual("CM", Romans.ToRoman(900));
+        }
+
+        [Test]
+        public void ShouldConvertAnyDecimalToRoman()
+        {
+            Assert.AreEqual("II", Romans.ToRoman(2));
+            Assert.AreEqual("III", Romans.ToRoman(3));
+            Assert.AreEqual("VI", Romans.ToRoman(6));
+            Assert.AreEqual("VII", Romans.ToRoman(7));
+            Assert.AreEqual("VIII", Romans.ToRoman(8));
+            Assert.AreEqual("XV", Romans.ToRoman(15));
+            Assert.AreEqual("XXIX", Romans.ToRoman(29));
+            Assert.AreEqual("XLV", Romans.ToRoman(45));
+            Assert.AreEqual("XLIX", Romans.ToRoman(49));
+            Assert.AreEqual("MMMDCCCLXXXVIII", Romans.ToRoman(3888));
+            Assert.AreEqual("MMMMCMXCIX", Romans.ToRoman(4999));
+        }
+
+        [Test]
+        public void ShouldConvertSimpleRomansToDecimals()
+        {
+            Assert.AreEqual(1, Romans.ToDecimal("I"));
+            Assert.AreEqual(10, Romans.ToDecimal("X"));
+            Assert.AreEqual(40, Romans.ToDecimal("XL"));
+            Assert.AreEqual(900, Romans.ToDecimal("CM"));
+        }
+
+        [Test]
+        public void ShouldConvertAllRomansToDecimals()
+        {
+            Assert.AreEqual(2, Romans.ToDecimal("II"));
+            Assert.AreEqual(6, Romans.ToDecimal("VI"));
+            Assert.AreEqual(1100, Romans.ToDecimal("MC"));
+            Assert.AreEqual(900, Romans.ToDecimal("CM"));
+            Assert.AreEqual(901, Romans.ToDecimal("CMI"));
+        }
+    }
+}
